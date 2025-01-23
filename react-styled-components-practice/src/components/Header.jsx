789333e0c15dee88fe41a33a4f333d0cf2ecdd70
richmondafari <afaririchmond1000@gmail.com>
2025-01-23 16:33:23 +0000

    Pass props in styled components

---
+++ react-styled-components-practice/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { StyleHeader } from "./styles/Header.Styled";
+const Header = () => {
+  return (
+    <StyleHeader bg='red'>
+      {" "}
+      <h1>This is a header! </h1>
+    </StyleHeader>
+  );
+};
+
+export default Header;
---
+++ react-styled-components-practice/src/components/styles/Header.Styled.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyleHeader = styled.div`
+  background-color: ${ (props) => props.bg};
+  padding: 40px 0;
+
+
+`;
